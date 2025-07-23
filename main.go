cba9be2ac09767098b64901bac831a7f8144faed
Luis-E-Ortega <lorte030@gmail.com>
2025-07-23 18:15:04 -0400

    Added handler for assets, created directory for it, added image

--- main.go
+++ main.go
@@ -5,15 +5,24 @@ import (
 )
 
 func main() {
+	// Initialize multiplexer
 	mux := http.NewServeMux()
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":8080", // Set for localhost
 		Handler: mux,
 	}
+
+	// Adds handler for the root path
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	// Adds handler for assets
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 
 	err := server.ListenAndServe()
 	if err != nil {
 		println("error starting listen and serve: ", err.Error())
 		return
 	}
+
+	// To add handler for the root path
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 }
