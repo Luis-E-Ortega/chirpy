9a3e8260c82767369e1c39a7ef2a2ebbe21a3192
Luis-E-Ortega <lorte030@gmail.com>
2025-07-28 23:06:52 -0400

    Added writeHits function and registered it

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"sync/atomic"
 )
@@ -17,10 +18,15 @@ func main() {
 		Handler: mux,
 	}
 
+	// Initialize instance of apiConfig struct
+	apiCfg := apiConfig{}
+
 	// Handle requests to /healthz with the readiness check
 	mux.HandleFunc("/healthz", handleHealthz)
 	// Serve static files from /app/ path
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	// Serve metrics from /metrics path
+	mux.HandleFunc("/metrics/", apiCfg.writeHits)
 
 	err := server.ListenAndServe()
 	if err != nil {
@@ -35,9 +41,15 @@ func handleHealthz(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+// Increments and keeps track of count per request to server
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
+}
+
+func (cfg *apiConfig) writeHits(w http.ResponseWriter, req *http.Request) {
+	str := fmt.Sprintf("Hits %v", cfg.fileserverHits.Load())
+	w.Write([]byte(str))
 }
